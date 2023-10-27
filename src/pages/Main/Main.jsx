5b4ae532dcead3b56301708a49b3f5913c8ec4ec
cipher <cipher@gmail.com>
2023-10-27 19:25:38 +0500

    git init

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import NewsBanner from "../../components/NewsBanner/NewsBanner";
+import styles from "./styles.module.css";
+import { getNews } from "../../api/apiNews";
+import { useState } from "react";
+import NewsList from "../../components/NewsList/NewsList";
+const Main = () => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      try {
+        const response = await getNews();
+        setNews(response.news);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchNews();
+  }, []);
+  console.log(news);
+  return (
+    <main className={styles.main}>
+      {news.length > 0 ? <NewsBanner item={news[0]} /> : null}
+
+      <NewsList news={news}></NewsList>
+    </main>
+  );
+};
+
+export default Main;
